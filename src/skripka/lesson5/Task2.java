1420047637da05824b4b72e395b35a34d4966bb1
NastyaSkripka <skripka_nastena@bk.ru>
2022-04-04 15:11:45 +0300

    add task 7

--- src/skripka/lesson5/Task2.java
+++ src/skripka/lesson5/Task2.java
@@ -1,5 +1,30 @@
 package skripka.lesson5;
 
 public class Task2 {
+    public static void main(String[] args) {
+        int[] mass = new int [50];
+        for(int i=0, j=1; j<=99; i++){
+            if(j%2==1){
+                mass[i]=j;
+                j+=2;
+            }
+            System.out.print(mass[i] + " ");
+        }
 
-}
+        System.out.println("\n");
+
+        int[] demass = new int[50];
+        for(int i=0, j=99; j>=1; i++){
+            if(j%2==1){
+                demass[i]=j;
+                j-=2;
+            }
+            System.out.print(demass[i] + " ");
+        }
+    }
+    }
+
+
+
+
+
--- src/skripka/lesson6/Task1.java
+++ src/skripka/lesson6/Task1.java
@@ -1,8 +1,59 @@
 package skripka.lesson6;
 
 public class Task1 {
+    public static void main(String[] args) {
+        Phone firstphone=new Phone();
+        Phone secondphone=new Phone();
+        Phone theredphone=new Phone();
+        firstphone.model="Sumsung";
+        firstphone.number=3456270;
+        firstphone.weight=125;
+        firstphone.displayInfo();
+        firstphone.receiveCall();
+        firstphone.getNumber();
+        secondphone.model="Iphone";
+        secondphone.number=5678894;
+        secondphone.weight=432;
+        secondphone.displayInfo();
+        secondphone.receiveCall();
+        secondphone.getNumber();
+        theredphone.model="Huawei";
+        theredphone.number=7986543;
+        theredphone.weight=345;
+        theredphone.displayInfo();
+        theredphone.receiveCall();
+        theredphone.getNumber();
 
+    }
+}
+class Phone{
+    int weight;
+    String model;
+    int number;
+    String name="Alex";
+    void displayInfo(){
+        System.out.printf( "Model: %s \tWeight: %d\nnumber: ",model,weight,number);
 
+    }
+     int receiveCall(int number){
+        System.out.println("Call"+number);
+    }
+    public void getNumber(){
+        System.out.println(number);
+    }
+public Phone(String model,int number,int weight){
+       this(number,model);
+       this.weight=weight;
 
+}
+public Phone(int number,String model){
+        this.number=number;
+        this.model=model;
+}
+public Phone(){
 
+}
+public void receiveCall(String name,int number){
+    System.out.println("name"+number);
+}
 }
